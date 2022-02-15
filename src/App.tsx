5e9405fdefda11d25cc12200a8e91b81662c9bf0
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-02-15 16:45:37 -0300

    adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import linkedin from './img/linkedin.svg'
+import discord from './img/discord.svg'
+import facebook from './img/facebook.svg'
+
+import logo from './img/balledevlogo.svg'
+import programador from './img/meninoProg.svg'
+
+import blobs from './img/blobs.svg'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App grid-template">
+      <div className='blobs'>
+        <img src={blobs} />
+      </div>
+      <header>
+        <div className='icones'>
+          <img src={linkedin} />
+          <img src={discord} />
+          <img src={facebook} />
+        </div>
+        <div className='logo'>
+          <img src={logo} />            
+        </div>
       </header>
+      <main>
+        <h1>O maior banco de devs do Brasil</h1>
+        <p>Nao importa se front ou back end, fazer networking e muito importante. Faça parte da maior comunidade de desenvolvedores brasileiros.</p>
+        <button>Entre agora</button>
+      </main>
+      <div className='programador'>
+        <img src={programador} />
+      </div>
     </div>
   );
 }
