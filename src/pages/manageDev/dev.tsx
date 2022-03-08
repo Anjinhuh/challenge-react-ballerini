f261ee7f8914c34c33189b3881b49bfee0419980
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-03-08 07:54:34 -0300

    :sparkles: adding modal add dev

--- src/pages/manageDev/dev.tsx
+++ src/pages/manageDev/dev.tsx
@@ -19,14 +19,18 @@ import './devpage.css'
 import api from '../../api/api'
 
 // NECESSITA DE ADICIONAR DEV E COLOCAR PARA VER MAIS
-function adicionaModal(){
-    const modal = document.querySelector('.modal-add-dev')
-    const actSty =  modal?.getAttribute
-    console.log(actSty)
-}
+
 
 export default function Dev(){
-     
+    const [modal, setModal] = useState(Boolean)
+    function modifyModal(){
+        setModal(true)
+        if(modal == true){
+            setModal(false)
+        } else if(modal == false){
+            setModal(true)
+        }
+    } 
     const [dev, setDev] = useState([])
 
     useEffect(() =>{
@@ -38,6 +42,7 @@ export default function Dev(){
     return(
         
         <div className="App">
+
             <div className='modal-delete-dev inactive'>
 
                 <div className='modal-delete-box'>
@@ -52,8 +57,9 @@ export default function Dev(){
                 </div>
 
             </div>
-
-            <div className='inactive '>
+                {modal 
+                    ? 
+            <div className='model-add-dev'>
                 <div className='modal-add-box'>
                     <p>Adicionar desenvolvedor</p>
                     <div className='modal-add-div'>
@@ -65,7 +71,7 @@ export default function Dev(){
                         <input/>
                     </div>
                     <div className='modal-add-div'>
-                        <p>Cargo</p>
+                        <p>Ocupação</p>
                         <input/>
                     </div>
                     <div className='modal-add-div'>
@@ -78,12 +84,17 @@ export default function Dev(){
                     </div>
 
                     <div className='modal-add-button'>
-                        <button className='modal-add-button-cancel'>Cancelar</button>
+                        <button className='modal-add-button-cancel' onClick={() => {modifyModal()}}>Cancelar</button>
                         <button className='modal-add-button-confirm'>Adicionar</button>
                     </div>
                 </div>
-                
             </div>
+                    : 
+            <div className='inactive '>
+            </div>
+                }
+            
+            
 
             
             <header className='headerDev'>
@@ -101,7 +112,7 @@ export default function Dev(){
                         <input placeholder='Buscar' />
                     </div>
                     <div className='buttonHeader'>
-                        <button onClick={() => {adicionaModal()}}>Adicionar dev</button>
+                        <button onClick={() => {modifyModal()}}>Adicionar dev</button>
                     </div>
                 </div>
             </header>
